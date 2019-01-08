ffbe69b7aa7366717db4667764b5723a186fc202
Clare McEwan <cfmcewan@gmail.com>
2019-01-08 21:32:17 +0000

    list added

--- src/views/display_view.js
+++ src/views/display_view.js
@@ -12,14 +12,36 @@ DisplayView.prototype.bindEvents = function(){
 
 DisplayView.prototype.render = function(instrument){
   const instrumentDiv = document.querySelector('#instrument-info');
-  const infoInstrumentName = document.createElement('p');
+
+  const infoInstrumentName = document.createElement('h2');
   infoInstrumentName.textContent = `${instrument.name}:`
+
   const inforInstrumentDescription = document.createElement('p');
   inforInstrumentDescription.textContent = `${instrument.description}`
+
+  const instrumentList = this.createInfoList(instrument);
+
   instrumentDiv.innerHTML = '';
   instrumentDiv.appendChild(infoInstrumentName);
   instrumentDiv.appendChild(inforInstrumentDescription);
+  instrumentDiv.appendChild(instrumentList);
 };
 
+DisplayView.prototype.createInfoList = function(instrument) {
+  const infoList = document.createElement('ul');
+
+  const liInstrumentA = this.createLi(`${instrument.instruments[0]} `, infoList);
+  const liInstrumentB = this.createLi(`${instrument.instruments[1]} `, infoList);
+  const liInstrumentC = this.createLi(`${instrument.instruments[2]} `, infoList);
+  const liInstrumentD = this.createLi(`${instrument.instruments[3]} `, infoList);
+  const liInstrumentE = this.createLi(`${instrument.instruments[4]} `, infoList);
+  return infoList;
+};
+
+DisplayView.prototype.createLi = function(textContent, ul) {
+  const li = document.createElement('li');
+  li.textContent = textContent;
+  ul.appendChild(li);
+};
 
 module.exports = DisplayView;
